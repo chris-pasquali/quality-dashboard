3067faa1c38df64bfc2d20d75a596e8b77eccb6f
Chris Pasquali <chrispasquali@pop-os.localdomain>
2021-08-30 19:24:34 +1000

    Create coming soon page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import logo from './logo.svg'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh',
+      fontFamily: 'roboto'
+    }}>
+      <img src={logo} alt="Quality Logo"/>
+      <h1 style={{
+        color: '#6EC177',
+        margin: 0
+      }}>Wah Gwan, Soon Come</h1>
+      <p>What's Up, Coming Soon</p>
     </div>
-  );
+  )
 }
 
 export default App;
