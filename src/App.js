4b97087a41056ceb74b4b6e12c5964c477954095
Chris Pasquali <chrispasquali@pop-os.localdomain>
2021-09-02 00:18:49 +1000

    changing app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter } from "react-router-dom";
 import "./App.css";
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import Footer from "./Components/Footer"
 import HearderComponent from "./Components/Header/HearderComponent";
 
